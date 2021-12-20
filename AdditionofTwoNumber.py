f318a6c1b74743515c36acc7ccf5abf9cd4622e6
Dhavalsinh Dhanaji Patil <76774786+dhavalsinhpatil@users.noreply.github.com>
2021-12-21 00:13:48 +0530

    Create AdditionofTwoNumber.py

---
+++ AdditionofTwoNumber.py
@@ -0,0 +1,20 @@
+#Accept Two Number and Perform the Addition
+def Addition(iValue1,iValue2):
+    result=0
+    result= iValue1+iValue2
+    return result
+  
+def main():
+    no1=0
+    no2=0
+    ret=0
+    print("Enter the Value 1 :")
+    no1 = int(input())
+
+    print("Enter the Value 2 :")
+    no2 = int(input())
+    ret=a.Addition(no1,no2)
+    print("Addition is :",ret)
+
+if __name__=="__main__":
+    main()
