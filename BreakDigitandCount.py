651b2c8818ebc6d1be314d784370f36346da8494
Dhavalsinh Dhanaji Patil <76774786+dhavalsinhpatil@users.noreply.github.com>
2021-12-21 01:37:42 +0530

    Create BreakDigitandCount.py

---
+++ BreakDigitandCount.py
@@ -0,0 +1,23 @@
+#Write a program which accept number from user and return number of digits in that number.
+#Input : 5187934 
+#Output : 7
+
+
+def DigitBreak(iValue):
+    iCnt=-1
+    while (iValue>0):
+        iDigit = iValue%10
+        iValue=int(iValue)/10
+        iCnt=iCnt+1
+    return iCnt
+
+def main():
+    no=0
+    iRet=0
+    print("Enter number :")
+    no = int(input())
+    iRet= DigitBreak(no)
+    print("The Count of Digit Is :",iRet)
+
+if _name=="main_":
+    main()
