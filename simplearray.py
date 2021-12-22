cc70e2e12e1571c0046c6e355552f77bf1db3230
Dhavalsinh Dhanaji Patil <76774786+dhavalsinhpatil@users.noreply.github.com>
2021-12-22 23:04:34 +0530

    Create simplearray.py

---
+++ simplearray.py
@@ -0,0 +1,20 @@
+#PYTHON ARRAY PROGRAM 
+# Input : Size =5  (1,2,3,4,5)
+# Entered Data is : [1, 2, 3, 4, 5]
+
+def main():
+    size=0
+    i=0
+    Data= []
+    
+    print("How Many elements do you want to enter : ")
+    size = int(input())
+    
+    print("Enter the elements :")
+    for i in range (size):
+        Data.append(int(input()))
+    
+    print("Entered Data is :", Data)
+
+if __name__=="__main__":
+    main()
