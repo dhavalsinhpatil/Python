4d70df790b7d6967cad415c1e5ecb5cdecd351ee
Dhavalsinh Dhanaji Patil <76774786+dhavalsinhpatil@users.noreply.github.com>
2021-12-24 23:05:39 +0530

    Create ListAddition.py

---
+++ ListAddition.py
@@ -0,0 +1,22 @@
+def ListAddition(iList):
+    sum=0
+    for i in range(len(iList)):
+        sum =sum+iList[i]
+    return sum
+
+def List(iValue):
+    myList = []
+    print("Enter The Elements :")
+    for i in range(iValue):
+        myList.append(int(input()))
+
+    iRet = ListAddition(myList)
+    print("The Addition of List :",iRet)
+
+def main():
+    print("Enter the Total number of List :")
+    N= int(input())
+    List(N)
+    
+if __name__ == "__main__":
+    main()
